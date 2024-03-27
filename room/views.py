1c786c0c47337482095e63ad2efec9a532e0e6c1
leoseis <eboscrib2@yahoo.com>
2024-03-27 16:41:00 +0100

    loppin thru messages

--- room/views.py
+++ room/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required            # importing
 from django.shortcuts import render
 
 
-from .models import Room
+from .models import Room, Message
 
 @login_required
 def rooms(request):
@@ -15,9 +15,10 @@ def rooms(request):
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)[0:25]
     
     return render(request, 'room/room.html',{
-        'room':room
+        'room':room, 'messages':messages
     })
     
 
